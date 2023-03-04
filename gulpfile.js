e64f4eb7d255524c4aacd1870aa72b4f3861f65c
Aftab Khalid <iffi.d.s@gmail.com>
2023-03-04 15:34:59 +0500

    new gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 'use strict';
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
-   return gulp.src('./src/scss/style.scss')
-   .pipe(concat('abc.scss'))
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('./dist'));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function(){
+  return gulp.src('src/scss/style.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('dist'))
 });
