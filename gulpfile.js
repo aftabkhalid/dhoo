7ccec8e3f9031dd6ec302415550f442f426199d0
Aftab Khalid <iffi.d.s@gmail.com>
2023-03-04 15:42:40 +0500

    fix new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-gulp.task('sass', function(){
-  return gulp.src('src/scss/style.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('dist'))
+gulp.task('styles', () => {
+    return gulp.src('src/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
 });
+
+
+gulp.task('default', gulp.series(['styles']));
