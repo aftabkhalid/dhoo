30bcffd1de0bbebcb2e4fdfddfec997f786e5d88
Aftab Khalid <iffi.d.s@gmail.com>
2023-03-04 16:32:29 +0500

    fix minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,13 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
 
 gulp.task('styles', () => {
-    return gulp.src('src/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist'));
 });
-
 
 gulp.task('default', gulp.series(['styles']));
