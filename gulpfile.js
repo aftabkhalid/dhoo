ea37418a9b1ad0688de90812f507ba200dc3f13c
Aftab Khalid <iffi.d.s@gmail.com>
2023-03-04 12:52:27 +0500

    new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,12 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+sass.compiler = require('node-sass');
 
-// gulpfile.js
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    sourcemaps = require("gulp-sourcemaps");
-
-//Paths
-var paths = {
-  sass: {
-    src: "src/scss/style.scss",
-    dest: "css"
-  },
-
-  bootstrap: {
-    src: "node_modules/bootstrap/scss/bootstrap.scss",
-    dest: "css"
-  },
-
-  // Easily add additional paths
-  // ,html: {
-  //  src: '...',
-  //  dest: '...'
-  // }
-};
-
-function style() {
-  return (
-    gulp
-      //.src([paths.sass.src, paths.bootstrap.src])
-      .src(paths.sass.src)
-      .pipe(sourcemaps.init())
-      .pipe(sass())
-      .on("error", sass.logError)
-      .pipe(postcss([autoprefixer(), cssnano()]))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(paths.sass.dest))
-  );
-}
-
-//
-
-exports.style = style;
+gulp.task('sass', function () {
+   return gulp.src('./src/scss/style.scss')
+   .pipe(concat('abc.scss'))
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./dist'));
+});
