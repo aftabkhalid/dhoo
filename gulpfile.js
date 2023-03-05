4cbd1459db0e0bcfa0812597609498b8ef2ecb08
Aftab Khalid <iffi.d.s@gmail.com>
2023-03-05 13:21:09 +0500

    fix structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,59 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssnano = require('gulp-cssnano');
+const concat = require('gulp-concat');
+
+// gulp.task('styles', () => {
+//   return gulp.src('src/scss/style.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(cssnano())
+//     .pipe(gulp.dest('dist'));
+// });
+
+// gulp.task('styles', () => {
+//   return gulp.src([paths.sass.src, paths.plugin.src])
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(cssnano())
+//     .pipe(gulp.dest('dist'));
+// });
+
+// gulp.task('default', gulp.series(['styles']));
+
+var paths = {
+  sass: {
+    src: "src/scss/style.scss",
+    dest: "dist"
+  },
+  plugin: {
+    src: "src/css/vendor/*.css",
+    dest: "dist"
+  },
+  colors: {
+    src: "src/scss/colors/grape.scss",
+    dest: "dist"
+  }
+};
 
 gulp.task('styles', () => {
-  return gulp.src('src/scss/style.scss')
+  return gulp.src([paths.sass.src])
     .pipe(sass().on('error', sass.logError))
     .pipe(cssnano())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series(['styles']));
+gulp.task('colors', () => {
+  return gulp.src([paths.colors.src])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist'));
+});
+
+function pluginTask() {
+  return gulp.src([paths.plugin.src])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cssnano())
+    .pipe(concat('plugin.css'))
+    .pipe(gulp.dest('dist'))
+}
+
+gulp.task('default', gulp.series(['styles'], [pluginTask], ['colors']));
